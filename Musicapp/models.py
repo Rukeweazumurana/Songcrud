2491c92015a504538bd076cadaf8c48e08dbd1ae
Rukeweazumurana <114624234+Rukeweazumurana@users.noreply.github.com>
2022-10-27 19:32:08 -0500

    Update models.py

--- Musicapp/models.py
+++ Musicapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+
+class Artiste(models.Model):
+    first_name=models.CharField(max_length=20)
+    last_name=models.CharField(max_length=20)
+    age=models.IntegerField()
+
+class Song(models.Model):
+    artiste = models.OneToOneField(Artiste, on_delete=models.CASCADE)
+    title=models.CharField(max_length=50)
+    date_released=models.DateField(default=datetime.today)
+    likes=models.IntegerField()
+    singer_id=models.IntegerField()
+
+class Lyric(models.Model):
+    song = models.OneToOneField(Song, on_delete=models.CASCADE)
+    content=models.CharField(max_length=500)
+    music_id=models.IntegerField()
